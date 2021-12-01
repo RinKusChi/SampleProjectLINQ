17cb0db698f52bb1f049978a41a3b3336aca2735
RinKusChi <meyuzak@gmail.com>
2021-12-01 22:40:27 +0300

    JWT START

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -28,7 +28,7 @@ namespace Business.Concrete
             _productDal = productDal;
             _categoryService = categoryService;
         }
-
+            [SecuredOperation("admin,editor")]
             [ValidationAspect(typeof(ProductValidator))]
             public IResult Add(Product product)
             {
---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities.Concrete
+{
+    public class OperationClaims:IEntity
+    {
+        [Key]
+        public int OperationId { get; set; }
+        public string OperationName { get; set; }
+    }
+}
